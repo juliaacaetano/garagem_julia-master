a5fef5163c87976c8d821f388868c5b850d07012
Julia Caetano <juliahelena.caetano@gmail.com>
2022-08-29 15:04:29 -0300

    commit2 - garagem

--- core/views.py
+++ core/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import  Marca, Categoria, Modelo, Ano
+from core.serializers import (
+    MarcaSerializer,
+    CategoriaSerializer,
+    ModeloSerializer,
+    AnoSerializer,
+)
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
+class ModeloViewSet(ModelViewSet):
+    queryset = Modelo.objects.all()
+    serializer_class = ModeloSerializer
+
+class AnoViewSet(ModelViewSet):
+    queryset = Ano.objects.all()
+    serializer_class = AnoSerializer
+
+    
